d0e9a8d5e3ddb8abc3183cb58786973500fd9f4c
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 13:02:23 -0700

    Combined valid operator and valid int tests.

--- calculator.py
+++ calculator.py
@@ -1,23 +1,23 @@
 import arithmetic
 
-def valid_operator(token):
-    if token in ["+", "-", "/", "*", "square", "cube", "pow", "mod", "q"]:
+def valid_operator(tokenList):
+    if tokenList[0] in ["+", "-", "/", "*", "square", "cube", "pow", "mod", "q"] and tokenList[1].isdigit() and tokenList[2].isdigit():
         return True
     else:
         print "I don't understand."
         return False
 
-def valid_ints(token1, token2):
-    if token1.isdigit() and token2.isdigit():
-        return True
-    else:
-        print "I don't understand."
-        return False        
+#def valid_ints(token1, token2):
+#    if token1.isdigit() and token2.isdigit():
+#        return True
+#    else:
+#        print "I don't understand."
+#        return False        
 
 while True: 
     input = raw_input("> ")
     tokens = input.split()
-    if valid_operator(tokens[0]):
+    if valid_operator(tokens):
 
    # try:
    #     tokens[1] = int(tokens[1])
@@ -27,7 +27,7 @@ while True:
 
         if tokens[0] == "q" or tokens[0] == "Q":
             break
-        if valid_ints(tokens[1],tokens[2]):
+        if valid_operator(tokens):
             tokens[1] = int(tokens[1])
             tokens[2] = int(tokens[2])
             if tokens[0] == "+":
