69702652f60d1318fdabe6e266178e70a6291df3
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 15:33:38 -0700

    Improved valid operator to require min two nums.

--- calculator.py
+++ calculator.py
@@ -1,8 +1,19 @@
 import arithmetic
 
 def valid_operator(tokenList):
-    if tokenList[0] in ["+", "-", "/", "*", "square", "cube", "pow", "mod", "q"] and tokenList[1].isdigit() and tokenList[2].isdigit():
-        return True
+    operators = ["+", "-", "/", "*", "square", "cube", "pow", "mod"]
+    print "Token list length: ",len(tokenList)
+    
+    if tokenList[0] in operators and len(tokenList) >= 3:
+        my_bool = True
+        #print "my_bool: %r" % my_bool
+        for x in range(1, len(tokenList)):
+            #print " x = %r" % x
+            #print " tokenList = %r" % tokenList[x]
+            if tokenList[x].isdigit() == False:
+                print "I don't understand."
+                my_bool = False
+        return my_bool
     else:
         print "I don't understand."
         return False
@@ -13,6 +24,7 @@ while True:
     if tokens[0] == "q" or tokens[0] == "Q":
         break
     if valid_operator(tokens):
+        # make a loop to int all the tokens
         tokens[1] = int(tokens[1])
         tokens[2] = int(tokens[2])
         if tokens[0] == "+":
