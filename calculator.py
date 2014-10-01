0ecfb238c3b8eea57dd8bc78c998c680b5282365
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 13:03:49 -0700

    Removed an extraneous loop

--- calculator.py
+++ calculator.py
@@ -7,28 +7,13 @@ def valid_operator(tokenList):
         print "I don't understand."
         return False
 
-#def valid_ints(token1, token2):
-#    if token1.isdigit() and token2.isdigit():
-#        return True
-#    else:
-#        print "I don't understand."
-#        return False        
-
 while True: 
     input = raw_input("> ")
-    tokens = input.split()
+    tokens = input.split() 
+    if tokens[0] == "q" or tokens[0] == "Q":
+        break
     if valid_operator(tokens):
-
-   # try:
-   #     tokens[1] = int(tokens[1])
-   #     tokens[2] = int(tokens[2])
-   # except ValueError:
-   #     print "I don't understand."
-
-        if tokens[0] == "q" or tokens[0] == "Q":
-            break
-        if valid_operator(tokens):
-            tokens[1] = int(tokens[1])
-            tokens[2] = int(tokens[2])
-            if tokens[0] == "+":
-                print arithmetic.add(tokens[1],tokens[2])
+        tokens[1] = int(tokens[1])
+        tokens[2] = int(tokens[2])
+        if tokens[0] == "+":
+            print arithmetic.add(tokens[1],tokens[2])
